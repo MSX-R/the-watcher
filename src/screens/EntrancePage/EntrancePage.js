c980988ad6353ffd83d81b680440021ea13c4a92
Marsaleix Romain <msx.coach@gmail.com>
2022-08-29 20:22:41 +0200

    entrance page added

--- src/screens/EntrancePage/EntrancePage.js
+++ src/screens/EntrancePage/EntrancePage.js
@@ -1,7 +1,23 @@
 import React from "react";
 import "./EntrancePage.css";
+import Logo from "../../assets/logoBlanc.png";
+
+import { useNavigate } from "react-router-dom";
+
 const EntrancePage = () => {
-  return <div>EntrancePage</div>;
+    const navigate = useNavigate();
+
+
+  return (
+    <div className="entrance">
+    <img
+      src={Logo}
+      alt="Logo"
+      className="logo HeartbeatAnim"
+      onClick={() => navigate("/home")}
+    />
+  </div>
+  )
 };
 
 export default EntrancePage;
